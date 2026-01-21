275bc20b5824007e76d847bbd75e9e879b798cd4
Nelson <punkpunkada@gmail.com>
2026-01-21 19:10:47 -0300

    conditional render

--- src/App.tsx
+++ src/App.tsx
@@ -1,124 +1,38 @@
-import { useEffect, useState, useRef, useMemo, useCallback } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [tasks, setTasks] = useState<string[]>([]);
-  const [input , setInput] = useState("");
-  const [editTask, setEditTask] = useState({
-    enabled: false,
-    task: ''
-  })
+  //const [signed, setSigned] = useState(false);
 
-  const inputRef = useRef<HTMLInputElement>(null);
-  const firstRender = useRef(true);
+  const [username, setUsername] = useState("Nelson");
 
-  useEffect(()=>{
-    const tasksLoaded = localStorage.getItem("Tarefas");
-    if(tasksLoaded){
-      setTasks(JSON.parse(tasksLoaded));
-    }
-    console.log(tasksLoaded);
-    
-  },[]);
-
-  useEffect(()=>{
-    if(firstRender.current){
-      firstRender.current = false
-      return
-    }
-  
-    localStorage.setItem("Tarefas", JSON.stringify(tasks));
-  },[tasks])
-
-  const handleRegister = useCallback(()=>{
-     inputRef.current?.focus();
-    
-
-    if(!input){
-      alert('Preencha sua trefa')
-      return
-    }
-
-    if(editTask.enabled){
-      handleSaveEdit();
-      return
-    }
-    setTasks(tasks => [...tasks, input])
-    setInput("")
-  },[input, tasks])
-
-  /* function handleRegister(){
-    inputRef.current?.focus();
-    
-
-    if(!input){
-      alert('Preencha sua trefa')
-      return
-    }
-
-    if(editTask.enabled){
-      handleSafeEdit();
-      return
-    }
-    setTasks(tasks => [...tasks, input])
-    setInput("")
-    //localStorage.setItem("Tarefas", JSON.stringify([...tasks, input]));
-    
-  } */
-
-
-  function handleSaveEdit(){
-    const findIndex = tasks.findIndex(task => task === editTask.task);
-    const allTasks = [...tasks];
-    allTasks[findIndex] = input;
-    setTasks(allTasks);
-    setEditTask({
-      enabled: false,
-      task: ''
-    });
-    setInput('');
-//    localStorage.setItem("Tarefas", JSON.stringify(allTasks));
-  }
-
-  function handleDelete(item: string){
-      const removetask = tasks.filter(task => task !== item)
-      setTasks(removetask);
-//      localStorage.setItem("Tarefas", JSON.stringify(removetask));
-  }
-  function handleEdit(item : string){
-    setInput(item)
-    setEditTask({
-      enabled: true,
-      task: item
-    });
-  }
-
-  const totalTask = useMemo(()=>{
-    return tasks.length
-  },[tasks]);
-
+ 
   return (
+
     <div>
-      <h1>Lista de Tarefas</h1>
-      <input 
-        ref={inputRef}
-        type="text"
-        placeholder='Digite o nome da tarefa'
-        value={input}
-        onChange={(e) => setInput(e.target.value)} />
-      <button onClick={handleRegister}>{editTask.enabled ?"Atualizar Tarefa": "Adicionar Tarefa"}</button>
-      <hr/>
-      <strong>Você tem {totalTask} tarefas</strong>
-      <br/><br/>
-      {tasks.map((item, index) =>(
-        <section key={index}>
-          <span>{item}</span>
-          <button onClick={()=> handleEdit(item)}>Editar</button>
-          <button onClick={()=> handleDelete(item)}>Excluir</button>
-          </section>
-      ))}
-    </div>
-  )
+      {username.length >= 5 && <h1>Username muito grande</h1>}
+    
+
+
+
+    {/*
+      <button onClick={()=> setSigned(true)}>ENTRAR</button>
+
+    {signed && <div>
+       <h1>Bem vindo Nelson</h1>
+       <p>usuario online!</p><br/>
+      <button onClick={()=> setSigned(false)}>SAIR</button>
+
+      </div>} */}
+
+    {/*       {signed ? (
+        <h1>Bem vindo Nelson</h1>
+      ):(
+        <h1>Nenhum usuario online</h1> 
+        
+      )} */}
+      </div>
+  );
 }
 
 export default App;
